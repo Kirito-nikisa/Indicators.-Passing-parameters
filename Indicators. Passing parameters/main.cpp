134bf37faa2490112ad6bd58698ed8a0176aceba
Kirito-nikisa <nikosa090@gmail.com>
2024-06-12 19:10:36 +0300

    Added lab ex1

--- Indicators. Passing parameters/main.cpp
+++ Indicators. Passing parameters/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
 
+void sumAndProduct(int* arr, int size, int* sumPtr, int* productPtr) {
+ 
+    *sumPtr = 0;
+    *productPtr = 1;
+
+ 
+    for (int i = 0; i < size; ++i) {
+        *sumPtr += arr[i];
+        *productPtr *= arr[i];
+    }
+}
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE] = { 1, 2, 3, 4, 5 };
+    int sum, product;
+
+   
+    sumAndProduct(arr, SIZE, &sum, &product);
+    cout << "Sum of elements: " << sum << endl;
+    cout << "Product of elements: " << product << endl;
+
+    return 0;
 }
