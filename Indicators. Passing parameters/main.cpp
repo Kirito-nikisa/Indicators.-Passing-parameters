13af1282f1843ae7acb41c8bd79882fdbd519599
Kirito-nikisa <nikosa090@gmail.com>
2024-06-23 00:11:29 +0300

    Addded lab ex7

--- Indicators. Passing parameters/main.cpp
+++ Indicators. Passing parameters/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
+void removeBlockFromArray(vector<int>& array, size_t startIndex, size_t count) {
+    if (startIndex >= array.size()) {
+        cout << "Start index is out of array bounds" << endl;
+        return;
+    }
 
+    size_t endIndex = min(startIndex + count, array.size());
+    array.erase(array.begin() + startIndex, array.begin() + endIndex);
+}
+
+int main() {
+    vector<int> array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    cout << "Initial array: ";
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    removeBlockFromArray(array, 4, 3);
+
+    cout << "Array after removal: ";
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
